5e994eba349d38c6f871f29a6f8a37093dfa7394
Haim Isakov <58917696+HaimIsakov@users.noreply.github.com>
2021-05-18 11:15:01 +0300

    change qgcn_activator to save the plots with data

--- Models/QGCN-master/QGCN/QGCN_model/qgcn_activator.py
+++ Models/QGCN-master/QGCN/QGCN_model/qgcn_activator.py
@@ -437,14 +437,13 @@ class QGCNActivator:
             self._update_auc(pred, true_labels, job=job)
         return loss
 
-    def plot_measurement(self, root, measurement=LOSS_PLOT):
-
+     def plot_measurement(self, root, date, measurement=LOSS_PLOT):
         if measurement == LOSS_PLOT:
             plt.plot(range(len(self._loss_vec_train)), self._loss_vec_train, label=TRAIN_JOB, color='b')
             plt.plot(range(len(self._loss_vec_dev)), self._loss_vec_dev, label=VALIDATE_JOB, color='r')
             plt.plot(range(len(self._loss_vec_test)), self._loss_vec_test, label=TEST_JOB, color='g')
             plt.legend(loc='best')
-            plt.savefig(os.path.join(root, f'loss_plot_{Date}.png'))
+            plt.savefig(os.path.join(root, f'loss_plot_{date}.png'))
             plt.clf()
 
         if measurement == ACCURACY_PLOT:
@@ -453,7 +452,7 @@ class QGCNActivator:
             plt.plot(range(len(self._accuracy_vec_dev)), self._accuracy_vec_dev, label=VALIDATE_JOB, color='r')
             plt.plot(range(len(self._accuracy_vec_test)), self._accuracy_vec_test, label=TEST_JOB, color='g')
             plt.legend(loc='best')
-            plt.savefig(os.path.join(root, f'acc_plot_{Date}_max_{round(max_acc_test, 2)}.png'))
+            plt.savefig(os.path.join(root, f'acc_plot_{date}_max_{round(max_acc_test, 2)}.png'))
             plt.clf()
 
         if measurement == AUC_PLOT:
@@ -462,18 +461,18 @@ class QGCNActivator:
             plt.plot(range(len(self._auc_vec_dev)), self._auc_vec_dev, label=VALIDATE_JOB, color='r')
             plt.plot(range(len(self._auc_vec_test)), self._auc_vec_test, label=TEST_JOB, color='g')
             plt.legend(loc='best')
-            plt.savefig(os.path.join(root, f'auc_plot_{Date}_max_{round(max_auc_test, 2)}.png'))
+            plt.savefig(os.path.join(root, f'auc_plot_{date}_max_{round(max_auc_test, 2)}.png'))
             plt.clf()
 
-    def plot_acc_loss_auc(self, root, save_model=True):
-        root = os.path.join(root, f'Qgcn_model_{Date}')
+    def plot_acc_loss_auc(self, root, date, save_model=True):
+        root = os.path.join(root, f'Qgcn_model_{date}')
         os.mkdir(root)
-        self.plot_measurement(root, LOSS_PLOT)
-        self.plot_measurement(root, ACCURACY_PLOT)
-        self.plot_measurement(root, AUC_PLOT)
+        self.plot_measurement(root, date, LOSS_PLOT)
+        self.plot_measurement(root, date, ACCURACY_PLOT)
+        self.plot_measurement(root, date, AUC_PLOT)
         # TODO: Fix the saving of the model
         if save_model:
-            model_path = os.path.join(root, f"qgcn_model_{Date}.p")
+            model_path = os.path.join(root, f"qgcn_model_{date}.p")
             pickle.dump(self.model().state_dict(), open(f"{model_path}", "wb"))
 
 
@@ -482,18 +481,22 @@ if __name__ == '__main__':
     from dataset.dataset_graphs_model import GraphsDataset
     
     # To run, choose one of these two codes - if you have external data choose the first one, else the second one
-    
+    # print(__file__)
     # If your data has an extenral information
     #params_file = "../params/default_binary_params.json"  # put here your params file
     params_file = "../params/microbiome_params.json"
     ext_train = ExternalData(params_file)
     ds = GraphsDataset(params_file, external_data=ext_train)
-    model = QGCN(params_file, ds.len_features, ext_train.len_embed())
-    activator = QGCNActivator(model, params_file, ds)
-    print("start training")
-    activator.train(show_plot=False, early_stop=True)
-    root_for_results = os.path.abspath(os.path.join(__file__, "../../../../../Results/QGCN"))
-    activator.plot_acc_loss_auc(root_for_results, save_model=False)
+    for i in range(10):
+        date = datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
+        print("--------------------------------------------------------------------------------------------------------------")
+        print(i)
+        model = QGCN(params_file, ds.len_features, ext_train.len_embed())
+        activator = QGCNActivator(model, params_file, ds)
+        print("start training")
+        activator.train(show_plot=False, early_stop=False)
+        root_for_results = os.path.abspath(os.path.join(__file__, "../../../../../Results/QGCN"))
+        activator.plot_acc_loss_auc(root_for_results, date, save_model=False)
 
     # If your data does not have external information
     # params_file = "../params/default_no_external_data_params.json"  # put here your params file
@@ -503,5 +506,6 @@ if __name__ == '__main__':
     # activator.train()
 
     # two_up =  os.path.abspath(os.path.join(__file__ ,"../.."))
+
     # "embeddings": [], "continuous": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125", "126", "127", "128", "129", "130", "131", "132", "133", "134", "135", "136", "137", "138", "139", "140", "141", "142", "143", "144", "145", "146", "147", "148", "149", "150", "151", "152", "153", "154", "155", "156", "157", "158", "159", "160", "161", "162", "163", "164", "165", "166", "167", "168", "169", "170", "171", "172", "173", "174", "175", "176", "177", "178", "179", "180", "181", "182", "183", "184", "185", "186", "187", "188", "189", "190", "191", "192", "193", "194", "195", "196", "197", "198", "199", "200", "201", "202", "203", "204", "205", "206", "207", "208", "209", "210", "211", "212", "213", "214", "215", "216", "217", "218", "219", "220", "221", "222", "223", "224", "225", "226", "227", "228", "229", "230", "231", "232", "233", "234", "235", "236", "237", "238", "239", "240", "241", "242", "243", "244", "245", "246", "247", "248", "249", "250", "251", "252", "253", "254", "255", "256", "257", "258", "259", "260", "261", "262", "263", "264", "265", "266", "267", "268", "269", "270", "271", "272", "273", "274", "275", "276", "277", "278", "279", "280", "281", "282"]},
 
